4684a41a5a38ddf17dc1127adf6ab5e8cf78f369
Rodrigo Moura <rodrigom.pe@outlook.com>
2021-02-23 10:06:19 -0300

    metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -59,6 +59,49 @@ class Usuario {
 		}
 
 	}
+	//Lista todos da tabela
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	}
+	//Busca um usuario especifico
+	public static function search($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"	
+		));
+
+	}
+	//Obtem os dados do usuário autenticado
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if (count($results) > 0) {
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new Datetime($row['dtcadastro']));
+
+		}else {
+
+			throw new Exception("Login e/ou senha invalidos.");
+
+		}
+
+	}
 
 	public function __toString(){
 
--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(2);
+//echo $root;
 
-$root->loadbyId(2);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("fe");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("user", "12345");
+
+echo $usuario;
 
  ?>
